ad416dfabb5535070b07f0c4b6fcdc1326c580a9
RINGOOOOOOOO <xuj678@newschool.edu>
2024-02-08 21:23:09 -0500

    Initial commit

---
+++ wxdy.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const fadingImage = document.querySelector(".fading-image");
+  const fadingText = document.querySelector(".fading-text");
+
+  fadingImage.classList.add("fade-in");
+  fadingText.classList.add("fade-in");
+});
